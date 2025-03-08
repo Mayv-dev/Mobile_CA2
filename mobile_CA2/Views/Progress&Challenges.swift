11a5aa97ef45a945a2456ac8e86f43d761e718a9
NojusMat <d00249674@student.dkit.ie>
2025-03-08 18:55:22 +0000

    LaunchView&ContentViews

---
+++ mobile_CA2/Views/Progress&Challenges.swift
@@ -0,0 +1,18 @@
+//
+//  Progress&Challenges.swift
+//  mobile_CA2
+//
+//  Created by Student on 07/03/2025.
+//
+
+import SwiftUI
+
+struct Progress_Challenges: View {
+    var body: some View {
+        Text("Progress & Challanges")
+    }
+}
+
+#Preview {
+    Progress_Challenges()
+}
---
+++ mobile_CA2/Views/Statistics.swift
@@ -0,0 +1,18 @@
+//
+//  Statistics.swift
+//  mobile_CA2
+//
+//  Created by Student on 07/03/2025.
+//
+
+import SwiftUI
+
+struct Statistics: View {
+    var body: some View {
+        Text("Statistics")
+    }
+}
+
+#Preview {
+    Statistics()
+}
